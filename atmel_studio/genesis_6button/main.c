d3188b5a464015a358926ad2f38883a413c431cc
Robert Fenton Merrill <rfmerrill@berkeley.edu>
2022-08-28 08:39:53 -0700

    Use MODE on pin 3 to select 3-button mode

--- atmel_studio/genesis_6button/main.c
+++ atmel_studio/genesis_6button/main.c
@@ -119,7 +119,25 @@ int main(void)
     CCP = 0xD8;
     CLKCTRL_MCLKCTRLB = 0x0001;  // Prescaler enabled with 1/2.
 
-    PORTA_DIR = _BV(4) | _BV(7);  // PA4 and PA7 as outputs.
+    PORTA_DIR = _BV(4) | _BV(7);         // PA4 and PA7 as outputs.
+    PORTA_PIN5CTRL |= PORT_PULLUPEN_bm;  // Pin 5 pulled up.
+
+    pins_3button();
+
+    // Wait to make sure we give time for MODE to rise
+    th_wait();
+    th_wait();
+    th_wait();
+    th_wait();
+
+    if (0 == (PORTA_IN & _BV(5)))
+    {
+        // MODE button held, stay in 3-button forever
+        while (true)
+        {
+            // loop forever
+        }
+    }
 
     while (true)
     {
